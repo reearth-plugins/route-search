f8b82e3692b63bb18a4933ba02d94223050f63d2
asahina820 <hina.gntm@gmail.com>
2025-04-02 16:20:01 +0900

    refactor: replace css to preset css

--- route-sarch.js
+++ route-sarch.js
@@ -1,185 +1,26 @@
 reearth.ui.show(`
   <style>
-  body,
-  h2,
-  h3 {
-    margin: 0;
-    font-family: Arial, sans-serif;
-  }
-  
-  .container {
-    display: flex;
-    width: 290px;
-    flex-direction: column;
-    align-items: flex-start;
-    border-radius: 4px;
-    border: 1px solid #8b8b8b;
-    background: #fff;
-  }
-  
-  .label-text {
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 20px;
-  }
-  
-  /* header */
-  
-  .header {
-    display: flex;
-    padding: 12px;
-    justify-content: space-between;
-    align-items: center;
-    align-self: stretch;
-  }
-  
-  .header-title {
-    display: flex;
-    align-items: center;
-    gap: 8px;
-  }
-  
-  .title-text {
-    color: #000;
-    text-align: center;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 24px;
-  }
-  
-  .close-button {
-    width: 16px;
-    height: 16px;
-  }
-  
-  /* form */
-  .form-area {
-    display: flex;
-    padding: 8px 8px 12px 8px;
-    flex-direction: column;
-    align-items: flex-start;
-    align-self: stretch;
-  }
-  
-  /* form select-point */
-  .select-point-section {
-    display: flex;
-    padding: 8px 16px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-end;
-    gap: 8px;
-    align-self: stretch;
-  }
-  
-  .point-group {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex: 1 0 0;
-    align-self: stretch;
-  }
-  
-  .input-wrapper {
-    display: flex;
-    width: 163.649px;
-    height: 28px;
-    padding: 0 6px;
-    align-items: center;
-    border-radius: 4px;
-    border: 1px solid #8b8b8b;
-  }
-  
-  .input-wrapper button {
-    background: none;
-    border: none;
-    padding: 0;
-    cursor: pointer;
-  }
-  
-  .location-input {
-    border: none;
-    outline: 0;
-  }
-  
-  /* form select-move */
-  .select-move-section {
-    display: flex;
-    padding: 12px 16px;
-    align-items: flex-start;
-    gap: 43px;
-  }
-  
-  .radio-group {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 4px;
-  }
-  
-  input[type="radio"] {
-    margin-right: 5px;
-  }
-  
-  /* form search-button-area */
-  .search-button-area {
-    display: flex;
-    padding: 8px 12px;
-    align-items: flex-start;
-    gap: 4px;
-    align-self: stretch;
-  }
-  
-  .search-btn {
-    width: 50%;
-    height: 28px;
-    border-radius: 6px;
-    border: 1px solid #1a6ed8;
-    background: #1a6ed8;
-  }
-  
-  .search-btn span {
-    color: #fff;
-    text-align: center;
-    font-size: 12px;
-  }
-  
-  .search-btn:hover, .delete-btn:hover {
-    cursor: pointer;
-  }
-  
-  .delete-btn {
-    width: 50%;
-    height: 28px;
-    border-radius: 6px;
-    border: 1px solid #1a6ed8;
-    background: #fff;
-  }
-  
-  .delete-btn span {
-    color: #1a6ed8;
-    text-align: center;
-    font-size: 12px;
-  }
-  
+  /* Generic styling system that provides consistent UI components and styling across all plugins */
+
+  @import url("https://reearth.github.io/visualizer-plugin-sample-data/public/css/preset-ui.css");
   </style>
-  <div class="container">
-    <div class="header">
-      <div class="header-title">
+  <div class="rounded-sm secondary-background" style="width: 290px">
+    <div class="display-flex flex-between p-16" style="height: 48px">
+      <div class="display-flex align-center gap-8">
         <img src="https://api.visualizer.reearth.io/assets/01jq5jw37s3n31dn3r894vhbrx.svg" width="20" height="20" />
-        <h2 class="title-text">経路検索</h2>
+        <h2 class="text-md">経路検索</h2>
       </div>
-      <div class="close-button">
+      <div>
         <img src="https://api.visualizer.reearth.io/assets/01jq5jwamr4j1psx403yfzg3yq.svg" width="20" height="20" />
       </div>
     </div>
-    <div class="form-area">
-      <div class="select-point-section">
-        <div class="point-group">
-          <span class="label-text" for="destination">開始点</span>
-          <div class="input-wrapper">
-            <input class="location-input" id="start-point" type="text" placeholder="場所を入力" />
-            <button onclick="handleIconClick('start', this)">
+    <div class="flex-column p-8">
+      <div class="flex-column gap-8 p-16">
+        <div class="flex-between" style="gap: 0">
+          <label class="font-bold" for="start-point">開始点</label>
+          <div class="display-flex" style="width: 180px; height: 30px; border-radius: 4px; border: 1px solid #8b8b8b">
+            <input id="start-point" type="text" placeholder="場所を入力" style="border: none; outline: 0" />
+            <button class="icon-btn" onclick="handleIconClick('start', this)">
               <img
                 src="https://api.visualizer.reearth.io/assets/01jq5k03t33w70121atw85n2ed.svg"
                 width="20"
@@ -188,11 +29,11 @@ reearth.ui.show(`
             </button>
           </div>
         </div>
-        <div class="point-group">
-          <span class="label-text" for="destination">到着点</span>
-          <div class="input-wrapper">
-            <input class="location-input" id="end-point" type="text" placeholder="場所を入力" />
-            <button onclick="handleIconClick('end', this)">
+        <div class="flex-between" style="gap: 0">
+          <label class="font-bold" for="end-point">到着点</label>
+          <div class="display-flex" style="width: 180px; height: 30px; border-radius: 4px; border: 1px solid #8b8b8b">
+            <input id="end-point" type="text" placeholder="場所を入力" style="border: none; outline: 0" />
+            <button class="icon-btn" onclick="handleIconClick('end', this)">
               <img
                 src="https://api.visualizer.reearth.io/assets/01jq5k03t33w70121atw85n2ed.svg"
                 width="20"
@@ -202,18 +43,21 @@ reearth.ui.show(`
           </div>
         </div>
       </div>
-      <div class="select-move-section">
-        <span class="label-text" for="move">移動方法</span>
-        <div class="radio-group">
-          <label
-            ><input type="radio" name="transport" value="foot" checked /><span class="label-text">徒歩</span></label>
-          <label><input type="radio" name="transport" value="bike" /><span class="label-text">自転車</span></label>
-          <label><input type="radio" name="transport" value="car" /><span class="label-text">自動車</span></label>
+
+      <div class="display-flex p-16" style="align-items: flex-start; gap: 43px">
+        <label class="font-bold">移動方法</label>
+        <div class="flex-column gap-4">
+          <div class="display-flex">
+            <label><input type="radio" name="transport" value="foot" checked style="width: 13px" /> 徒歩</label>
+          </div>
+          <label><input type="radio" name="transport" value="bike" style="width: 13px" />自転車</label>
+          <label><input type="radio" name="transport" value="car" style="width: 13px" />自動車</label>
         </div>
       </div>
-      <div class="search-button-area">
-        <button class="search-btn" onclick="searchRoute()"><span>検索</span></button>
-        <button class="delete-btn" onclick="deleteRouteAndMarker()"><span>削除</span></button>
+
+      <div class="display-flex gap-8 p-8">
+        <button class="btn-primary button-padding w-full text-sm" onclick="searchRoute()">検索</button>
+        <button class="btn-neutral button-padding w-full text-sm" onclick="deleteRouteAndMarker()">削除</button>
       </div>
     </div>
   </div>
